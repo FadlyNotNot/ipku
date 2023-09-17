575fe47fd71cb9658313b9aa256980d0591b4f61
FadlyNotNot <138890655+FadlyNotNot@users.noreply.github.com>
2023-09-17 15:18:00 +0700

    Update notif.sh

--- notif.sh
+++ notif.sh
@@ -5,6 +5,22 @@ z="\e033[93m"
 y="\e033[97"
 clear
 echo -e "${z}┌────────────────────────────────────────┐${x}"
+echo -e "${z}│                 PILIH OPTION                  "
+echo -e "${z}└────────────────────────────────────────┘${x}"
+echo -e ""
+echo -e "  [1] ADD NOTIF 
+echo -e "  [2] KIRIM NOTIF
+echo -e ""
+echo -e "${z}└────────────────────────────────────────┘${x}" 
+read -p " Pilih Option ( 1 - 2 ) " notif
+case $notif in
+1) clear ; notif ;;
+2) clear ; kirim ;;
+*) clear ; notif.sh ;;
+clear
+
+function notif() {
+echo -e "${z}┌────────────────────────────────────────┐${x}"
 echo -e "${z}│                 MASUKAN TOKEN                  "
 echo -e "${z}└────────────────────────────────────────┘${x}"
 echo ""
@@ -18,7 +34,12 @@ KEY="$Token"
 TIME="10"
 JAM="$(date +'%Y-%m-%d')"
 WKT="$(date +'%H:%M:%S')"
+}
+notif.sh
 clear
+
+
+function kirim() {
 echo -e "${z}┌────────────────────────────────────────┐${x}"
 echo -e "${z}│                 INPORT DATA MU                 "
 echo -e "${z}└────────────────────────────────────────┘${x}"
@@ -30,18 +51,22 @@ read -rp "     User Quota " : -e LIQ
 read -rp "     Expired " : -e exp
 echo -e ""
 echo -e "${z}└────────────────────────────────────────┘${x}"
+}
+notif.sh
 clear
+
+
 URL="https://api.telegram.org/bot$KEY/sendMessage"
 TEXT="
 <code>────────────────────</code>
-<b> TRANSAKSI VMESS</b>
+<b> ⚡NOTIF TRANSAKSI VMESS⚡ </b>
 <code>────────────────────</code>
 Username : <code>$Login</code>
-Harga   : <code>$Harga</code>
-Server : <code>$ISP</code>
-User IP   : <code>$LIP</code>
+Exp      : <code>$exp</code>
+Harga    : <code>$Harga</code>
+Server   : <code>$ISP</code>
+User IP  : <code>$LIP</code>
 User Quota  : <code>LIQ</code>
-Exp   : <code>$exp</code>
 <code>────────────────────</code>
 <i>Nontifikasi Otomatis Dari FV STORE Bot</i>
 "'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ⚡","url":"https://t.me/fdlyvpn_ID"}]]}'
